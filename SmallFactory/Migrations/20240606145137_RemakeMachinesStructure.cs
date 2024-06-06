592961510d431c98a00bfbe7e806d40dfed55503
yvrxslvw <yvrxslvv@gmail.com>
2024-06-06 16:52:22 +0200

    Remake machines model structure

---
+++ SmallFactory/Migrations/20240606145137_RemakeMachinesStructure.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmallFactory.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemakeMachinesStructure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "input_1_count",
+                table: "machines");
+
+            migrationBuilder.DropColumn(
+                name: "input_2_count",
+                table: "machines");
+
+            migrationBuilder.DropColumn(
+                name: "input_3_count",
+                table: "machines");
+
+            migrationBuilder.DropColumn(
+                name: "input_4_count",
+                table: "machines");
+
+            migrationBuilder.DropColumn(
+                name: "output_count",
+                table: "machines");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "input_1_count",
+                table: "machines",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "input_2_count",
+                table: "machines",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "input_3_count",
+                table: "machines",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "input_4_count",
+                table: "machines",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "output_count",
+                table: "machines",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
--- SmallFactory/Models/Machine.cs
+++ SmallFactory/Models/Machine.cs
@@ -24,21 +24,6 @@ namespace SmallFactory.Models
         [Column("receipt_id")]
         public int ReceiptId { get; set; }
 
-        [Column("input_1_count")]
-        public int Input1Count { get; set; }
-
-        [Column("input_2_count")]
-        public int Input2Count { get; set; }
-
-        [Column("input_3_count")]
-        public int Input3Count { get; set; }
-
-        [Column("input_4_count")]
-        public int Input4Count { get; set; }
-
-        [Column("output_count")]
-        public int OutputCount { get; set; }
-
         public ProductionChain ProductionChain { get; set; }
 
         public Receipt Receipt { get; set; }
